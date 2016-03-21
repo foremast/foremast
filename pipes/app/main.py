341439db7539ea10dbe310e063cbd4527cabd761
Doug Campbell <dcampbell@gogoair.com>
2016-03-21 14:37:07 -0500

    added method comments

--- pipes/app/main.py
+++ pipes/app/main.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+
+#A script for creating an application in spinnaker.
+#Simply looks to see if the application already exists, if not, creates
+
 import json
 import logging
 import os
@@ -15,6 +20,7 @@ class SpinnakerApp:
         if appinfo:
             self.appname = appinfo['name']
 
+    '''Gets all applications from spinnaker'''
     def get_apps(self):
         url = self.gate_url + "/applications"
         r = requests.get(url)
@@ -24,6 +30,7 @@ class SpinnakerApp:
             logging.error(r.text)
             sys.exit(1)
 
+    '''Checks to see if application already exists'''
     def app_exists(self):
         self.get_apps()
         for app in self.apps:
@@ -33,6 +40,7 @@ class SpinnakerApp:
         logging.info('{} does not exist...creating'.format(self.appname))
         return False
             
+    '''Uses jinja2 to setup POST data for appliucation creation'''
     def setup_appdata(self):
         curdir = os.path.dirname(os.path.realpath(__file__))
         templatedir = "{}/../../templates".format(curdir)
@@ -40,6 +48,8 @@ class SpinnakerApp:
         template = jinjaenv.get_template("app_data_template.json")
         print(template.render(appinfo=appinfo))
 
+
+    '''Sends a POST to spinnaker to create a new application'''
     def create_app(self):
         if not (self.app_exists()):
             url = "{}/applications/{}/tasks".format(self.gate_url, self.appname)
