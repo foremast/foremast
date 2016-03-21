c418f40b789d77ab8ba7889b9978ddc2e6fd08af
Doug Campbell <dcampbell@gogoair.com>
2016-03-21 14:58:54 -0500

    added argparse and appinfo dict construction. Fixed templating quotes also

--- pipes/app/main.py
+++ pipes/app/main.py
@@ -7,6 +7,7 @@ import json
 import logging
 import os
 import sys
+import argparse
 
 from jinja2 import Environment, FileSystemLoader
 import requests
@@ -60,8 +61,28 @@ class SpinnakerApp:
             return
 
 if __name__ == "__main__":
-    appinfo = { "name": "DougTest", "email": "dcampbell@example.com",
-            "project": "DougTest", "repo": "DougRepo", "Description": "this is a test"}
+    #Setup parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--name", help="The application name to create")
+    parser.add_argument("--email", help="Email address to associate with application",
+                        default="PS-DevOpsTooling@example.com")
+    parser.add_argument("--project", help="The project to associaste with application",
+                        default="None")
+    parser.add_argument("--repo", help="The repo to associaste with application",
+                        default="None")
+    parser.add_argument("--description", help="The application description",
+                        default="None")
+    parser.add_argument("-d", "--debug", action="store_true",
+                        help="Turns on full debugging", default=True)
+    args = parser.parse_args()
+
+    #Dictionary containing application info. This is passed to the class for processing
+    appinfo = { "name": args.name, 
+                "email": args.email,
+                "project": args.project, 
+                "repo": args.repo, 
+                "description": args.description } 
+
     spinnakerapps = SpinnakerApp(appinfo=appinfo)
     spinnakerapps.create_app()
 
