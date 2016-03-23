e813d2c397a7cf434065e5fbb84ac1e14855aa47
Sijis Aviles <saviles@gogoair.com>
2016-03-23 16:09:06 -0500

    Add initial port of dns
    Related: PSOBAT-1115

---
+++ pipes/dns/create_dns.py
@@ -0,0 +1,237 @@
+"""DNS functions for deployment."""
+import logging
+import sys
+from pprint import pformat
+import argparse
+import os
+import configparser
+
+import requests
+import boto3.session
+
+class SpinnakerAppNotFound(Exception):
+    pass
+
+
+class SpinnakerApplicationListError(Exception):
+    pass
+
+
+class SpinnakerDnsCreationFailed(Exception):
+    pass
+
+
+class SpinnakerDns:
+    """Manipulate Spinnaker Dns.
+
+    Args:
+        app_name: Str of application name add Security Group to.
+    """
+
+    def __init__(self, app_info):
+        self.log = logging.getLogger(__name__)
+
+        self.here = os.path.dirname(os.path.realpath(__file__))
+        self.config = self.get_configs()
+        self.gate_url = self.config['spinnaker']['gate_url']
+        self.app_name = self.app_exists(app_name=app_info['name'])
+
+        # Add domain
+        app_info.update({'domain': 'example.com'})
+        self.app_info = app_info
+
+        self.header = {'content-type': 'application/json'}
+
+        env = boto3.session.Session(
+            profile_name=self.app_info['environment'])
+        self.r53client = env.client('route53')
+
+
+    def get_configs(self):
+        """Get main configuration.
+
+        Returns:
+            configparser.ConfigParser object with configuration loaded.
+        """
+        config = configparser.ConfigParser()
+        configpath = "{}/../../configs/spinnaker.conf".format(self.here)
+        config.read(configpath)
+
+        self.log.debug('Configuration sections found: %s', config.sections())
+        return config
+
+    def get_template(self, template_name='', template_dict=None):
+        """Get Jinja2 Template _template_name_.
+
+        Args:
+            template_name: Str of template name to retrieve.
+            template_dict: Dict to use for template rendering.
+
+        Returns:
+            Dict of rendered JSON to send to Spinnaker.
+        """
+        templatedir = "{}/../../templates".format(self.here)
+        jinja_env = Environment(loader=FileSystemLoader(templatedir))
+        template = jinja_env.get_template(template_name)
+
+        rendered_json = json.loads(template.render(**template_dict))
+        self.log.debug('Rendered template: %s', rendered_json)
+        return rendered_json
+
+    def get_apps(self):
+        """Gets all applications from spinnaker."""
+        url = '{0}/applications'.format(self.gate_url)
+        r = requests.get(url)
+        if r.ok:
+            return r.json()
+        else:
+            logging.error(r.text)
+            raise SpinnakerApplicationListError(r.text)
+
+    def app_exists(self, app_name):
+        """Checks to see if application already exists.
+
+        Args:
+            app_name: Str of application name to check.
+
+        Returns:
+            Str of application name
+
+        Raises:
+            SpinnakerAppNotFound
+        """
+
+        apps = self.get_apps()
+        app_name = app_name.lower()
+        for app in apps:
+            if app['name'].lower() == app_name:
+                logging.info('Application %s found!', app_name)
+                return app_name
+
+        logging.info('Application %s does not exist ... exiting', app_name)
+        raise SpinnakerAppNotFound('Application "{0}" not found.'.format(
+            app_name))
+
+
+    def create_elb_dns(self):
+        """ Creates dns entries in route53
+
+        Args:
+            app_catalog: A dictionary containing all parameters.
+
+        Returns:
+            Auto-generated DNS name for the Elastic Load Balancer.
+        """
+
+        log = logging.getLogger(__name__)
+
+        app = self.app_name
+        dns_zone = '{environment}.{domain}'.format(**self.app_info)
+
+        app_fqdn = '{name}.stack.{environment}.{domain}'.format(**self.app_info)
+
+        # TODO: Get elb details and validate.
+        elb_dns_name = 'something.crazy.from.amazon'
+
+        print('||'.join([app_fqdn, elb_dns_name, dns_zone]))
+        #sys.exit()
+
+        # get correct hosted zone
+        zones = self.r53client.list_hosted_zones_by_name(
+            DNSName=dns_zone)
+        # log.debug('zones:\n%s', pformat(zones))
+
+        zone_ids = []
+        if len(zones['HostedZones']) > 1:
+            for zone in zones['HostedZones']:
+                log.debug('zone:\n%s', pformat(zone))
+
+                # We will always add a private record. The elb subnet must be
+                # specified as 'external' to get added publicly.
+                if zone['Config']['PrivateZone'] or \
+                                self.app_info['config']['elb']['subnet_purpose'] in (
+                                'external'):
+                    log.info('Adding DNS record to %s zone', zone['Id'])
+                    zone_ids.append(zone['Id'])
+
+        log.info('Updating Application URL: %s', app_fqdn)
+
+        # list the records in the zones
+        for zone_id in zone_ids:
+            records = self.r53client.list_resource_record_sets(HostedZoneId=zone_id,
+                                                       StartRecordName=app_fqdn,
+                                                       StartRecordType='CNAME')
+            name_list = [record['Name'] for record in
+                         records['ResourceRecordSets']]
+            log.log(15, 'Zone %s CNAME record names=%s', zone_id, name_list)
+
+        # This is what will be added to DNS
+        changes = {
+            'Comment': 'automatic change',
+            'Changes': [{
+                'Action': 'UPSERT',
+                'ResourceRecordSet': {
+                    'Name': app_fqdn,
+                    'Type': 'CNAME',
+                    'TTL': 300,
+                    'ResourceRecords': [{'Value': elb_dns_name}, ]
+                },
+            }, ],
+        }
+
+        log.log(15, 'changes:\n%s', pformat(changes))
+
+        for zone_id in zone_ids:
+
+            log.debug('zone_id: %s', zone_id)
+            """
+            response = self.r53client.change_resource_record_sets(HostedZoneId=zone_id,
+                                                          ChangeBatch=changes, )
+            log.log(15, 'response:\n%s', pformat(response))
+            """
+        return app_fqdn
+
+def main():
+    """Run newer stuffs."""
+    logging.basicConfig(format='%(asctime)s %(message)s')
+    log = logging.getLogger(__name__)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d',
+                        '--debug',
+                        action='store_true',
+                        help='DEBUG output')
+    parser.add_argument("--name",
+                        help="The application name to create",
+                        required=True)
+    parser.add_argument("--region",
+                        help="The region to create the security group",
+                        required=True)
+    parser.add_argument("--vpc",
+                        help="The vpc to create the security group",
+                        required=True)
+    parser.add_argument("--environment",
+                        help="The environment to create the security group",
+                        required=True)
+    args = parser.parse_args()
+
+    if args.debug:
+        log.setLevel(logging.DEBUG)
+
+    log.debug('Parsed arguments: %s', args)
+
+    #Dictionary containing application info. This is passed to the class for processing
+    appinfo = {'name': args.name,
+               'vpc': args.vpc,
+               'region': args.region,
+               'environment': args.environment,}
+
+    # TODO: Get actual items from application.json
+    appinfo.update({'config': {'elb': {'subnet_purpose': 'internal'}}})
+
+    spinnakerapps = SpinnakerDns(app_info=appinfo)
+    spinnakerapps.create_elb_dns()
+
+
+if __name__ == "__main__":
+    main()
