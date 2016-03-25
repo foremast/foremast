572615677d94b3a9fab41733d1e505b4cc80c4d0
Sijis Aviles <saviles@gogoair.com>
2016-03-25 14:16:40 -0500

    Add build as available environment

--- pipes/iam/__main__.py
+++ pipes/iam/__main__.py
@@ -20,7 +20,7 @@ def main():
                         help='Set DEBUG output')
     parser.add_argument('-e',
                         '--env',
-                        choices=('dev', 'stage', 'prod'),
+                        choices=('build', 'dev', 'stage', 'prod'),
                         default='dev',
                         help='Deploy environment')
     parser.add_argument('-a',
