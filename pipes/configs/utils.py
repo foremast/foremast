1f5d15def1cc2e14f2d36a0c76babfb5034bf040
Nate Tangsurat <ntangsurat@gogoair.com>
2016-04-02 01:24:50 -0500

    fix: Use ChainMap with deep resolution
    Need to override the default behaviour of collections.ChainMap to allow
    for nested dictionary lookups.

--- pipes/configs/utils.py
+++ pipes/configs/utils.py
@@ -1,3 +1,4 @@
+import collections
 import configparser
 import logging
 import os
@@ -5,6 +6,41 @@ import os
 import jinja2
 
 LOG = logging.getLogger(__name__)
+
+
+class DeepChainMap(collections.ChainMap):
+    """Deep lookups for collections.ChainMap objects.
+
+    When there are nested dicts, the first found, second level dict is returned
+    instead of overlaying alternative second level dicts.
+
+        >>> first = {'key1': {'key1_1': 'first_one'}}
+        >>> second = {'key1': {'key1_1': 'second_one', 'key1_2': 'second_two'}}
+        >>> collections.ChainMap(first, second)['key1']
+        {'key1_1': 'first_one'}
+        >>> collections.ChainMap(second, first)['key1']
+        {'key1_1': 'second_one', 'key1_2': 'second_two'}
+
+        # Deep lookup will flatten every level.
+
+        >>> DeepChainMap(first, second)['key1']
+        {'key1_1': 'first_one', 'key1_2': 'second_two'}
+        >>> DeepChainMap(second, first)['key1']
+        {'key1_1': 'second_one', 'key1_2': 'second_two'}
+    """
+
+    def __getitem__(self, key):
+        for mapping in self.maps:
+            try:
+                value = mapping[key]
+                if isinstance(value, dict):
+                    return dict(DeepChainMap(*list(mapping.get(key, {})
+                                                   for mapping in self.maps)))
+                else:
+                    return value
+            except KeyError:
+                pass
+        return self.__missing__(key)
 
 
 def get_configs(file_name='spinnaker.conf'):
