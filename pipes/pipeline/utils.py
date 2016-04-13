0fd3ac951db49a755080241f05f66ea27b2bb590
Sijis Aviles <saviles@gogoair.com>
2016-04-13 15:33:19 -0500

    pep8 fixes

--- pipes/pipeline/utils.py
+++ pipes/pipeline/utils.py
@@ -1,18 +1,17 @@
 import requests
 
-
 def get_subnets(gate_url='http://gate-api.build.example.com:8084', target='ec2'):
     """ Gets all availability zones for a given target
-        Params: 
+        Params:
             gate_url: The URL to hit for gate API access
             target: the type of subnets to look up (ec2 or elb)
         Return:
-            az_dict: dictionary of  availbility zones, structured like 
+            az_dict: dictionary of  availbility zones, structured like
             { $account: { $region: [ $avaibilityzones ] } } """
     az_dict = {}
     subnet_url = gate_url + "/subnets/aws"
-    r =  requests.get(subnet_url)
-    
+    r = requests.get(subnet_url)
+
     for item in r.json():
         if item['target'] == target:
             az = item['availabilityZone']
@@ -25,8 +24,8 @@ def get_subnets(gate_url='http://gate-api.build.example.com:8084', target='ec2')
                 try:
                     az_dict[account][region].append(az)
                 except KeyError:
-                    az_dict[account] = {region: [az] }
-  
+                    az_dict[account] = {region: [az]}
+
     return az_dict
 
 
