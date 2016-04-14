0a948ec935bba9249939f5b493fb8a59a6eb769f
Doug Campbell <dcampbell@gogoair.com>
2016-04-14 11:07:29 -0500

    fixed dictionary region issue

--- pipes/pipeline/utils.py
+++ pipes/pipeline/utils.py
@@ -1,4 +1,5 @@
 import requests
+from collections import defaultdict 
 
 def get_subnets(gate_url='http://gate-api.build.example.com:8084', target='ec2'):
     """ Gets all availability zones for a given target
@@ -9,24 +10,26 @@ def get_subnets(gate_url='http://gate-api.build.example.com:8084', target='ec2')
             az_dict: dictionary of  availbility zones, structured like
             { $account: { $region: [ $avaibilityzones ] } } """
     az_dict = {}
+    az_list = []
+    account_az_dict = defaultdict(defaultdict)
     subnet_url = gate_url + "/subnets/aws"
     r = requests.get(subnet_url)
 
-    for item in r.json():
-        if item['target'] == target:
-            az = item['availabilityZone']
-            account = item['account']
-            region = item['region']
-            try:
-                if az not in az_dict[account][region]:
-                    az_dict[account][region].append(az)
-            except KeyError:
-                try:
-                    az_dict[account][region].append(az)
-                except KeyError:
-                    az_dict[account] = {region: [az]}
+    for subnet in r.json():
+        if subnet['target'] == target:
+            az = subnet['availabilityZone']
 
-    return az_dict
+            account = subnet['account']
+            region = subnet['region']
+            if account == 'pci':
+                print(az)
+            try:
+                if az not in account_az_dict[account][region]:
+                    account_az_dict[account][region].append(az)
+            except KeyError:
+                account_az_dict[account][region] = [az]
+
+    return account_az_dict
 
 
 if __name__ == "__main__":
