fe7c56545cf31e5fce5b039af41fed1431a05833
Nate Tangsurat <ntangsurat@gogoair.com>
2016-04-28 15:20:18 -0500

    fix: Use centralized HTTP HEADERS
    See also: PSOBAT-1399

--- src/foremast/consts.py
+++ src/foremast/consts.py
@@ -1,5 +1,9 @@
 """Package constants."""
 API_URL = 'http://gate-api.build.example.com:8084'
-HEADERS = {'content-type': 'application/json', 'user-agent': 'foremast'}
+HEADERS = {
+    'accept': '*/*',
+    'content-type': 'application/json',
+    'user-agent': 'foremast',
+}
 LOGGING_FORMAT = ('%(asctime)s [%(levelname)s] %(name)s:%(funcName)s:'
                   '%(lineno)d - %(message)s')
