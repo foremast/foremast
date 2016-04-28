632193b38d78770755016c9b1c4ab9fa534a2cb3
Nate Tangsurat <ntangsurat@gogoair.com>
2016-04-28 12:23:14 -0500

    fix: Move Health Check logic to class
    See also: PSOBAT-1399

--- src/foremast/elb/create_elb.py
+++ src/foremast/elb/create_elb.py
@@ -15,12 +15,33 @@ class SpinnakerELB:
     def __init__(self, args=None):
         self.args = args
 
+        self.health_path = ''
+        self.health_port = ''
+        self.health_proto = ''
+        self.set_health()
+
         self.here = os.path.dirname(os.path.realpath(__file__))
         self.templatedir = '{0}/../templates/'.format(self.here)
         jinjaenv = Environment(loader=FileSystemLoader(self.templatedir))
         self.elb_template = jinjaenv.get_template("elb_data_template.json")
         self.gate_url = "http://gate-api.build.example.com:8084"
         self.header = {'Content-Type': 'application/json', 'Accept': '*/*'}
+
+    def set_health(self):
+        """Set Health Check path, port, and protocol."""
+        target = self.args.health_target
+        self.health_proto, health_port_path = target.split(':')
+        self.health_port, *health_path = health_port_path.split('/')
+
+        if not health_path:
+            self.health_path = '/healthcheck'
+        else:
+            self.health_path = '/{0}'.format('/'.join(health_path))
+
+        LOG.info('Health Check\n\tprotocol: %s\n\tport: %s\n\tpath: %s',
+                 self.health_proto, self.health_port, self.health_path)
+
+        return True
 
     def create_elb(self, json_data, app):
         """Create/Update ELB.
