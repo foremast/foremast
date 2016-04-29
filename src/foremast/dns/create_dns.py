32f0a0e364fae1c7208a271a7554f6a4a5b15ba0
Nate Tangsurat <ntangsurat@gogoair.com>
2016-04-29 02:05:35 -0500

    refactor: Pass variables to template directly
    See also: PSOBAT-1197

--- src/foremast/dns/create_dns.py
+++ src/foremast/dns/create_dns.py
@@ -4,12 +4,11 @@ import logging
 from pprint import pformat
 
 import boto3.session
-import gogoutils
 import requests
 from tryagain import retries
 
 from ..consts import API_URL
-from ..exceptions import SpinnakerAppNotFound, SpinnakerElbNotFound
+from ..exceptions import SpinnakerElbNotFound
 from ..utils import get_app_details, get_template
 
 
@@ -23,7 +22,8 @@ class SpinnakerDns:
     def __init__(self, app_info):
         self.log = logging.getLogger(__name__)
 
-        self.app_name = get_app_details.get_details(app_info['app']).app_name()
+        self.generated = get_app_details.get_details(app_info['app'])
+        self.app_name = self.generated.app_name()
 
         # Add domain
         app_info.update({'domain': 'example.com'})
@@ -33,31 +33,6 @@ class SpinnakerDns:
 
         env = boto3.session.Session(profile_name=self.app_info['env'])
         self.r53client = env.client('route53')
-
-    def get_app_detail(self):
-        """Retrieve app details."""
-        url = '{0}/applications/{1}'.format(API_URL, self.app_name)
-        r = requests.get(url)
-
-        details = {}
-        if r.ok:
-            details.update(r.json())
-
-            group = details['attributes'].get('repoProjectKey')
-            project = details['attributes'].get('repoSlug')
-            generator = gogoutils.Generator(project=group,
-                                            repo=project,
-                                            env=self.app_info['env'])
-
-            details.update({'dns_elb': generator.dns()['elb']})
-            details.update({'dns_elb_aws': self.get_app_aws_elb()})
-        else:
-            raise SpinnakerAppNotFound('Application %s not found',
-                                       self.app_name)
-
-        self.log.debug('Application details: %s', details)
-
-        return details
 
     @retries(max_attempts=10, wait=10.0, exceptions=SpinnakerElbNotFound)
     def get_app_aws_elb(self):
@@ -92,7 +67,8 @@ class SpinnakerDns:
         """
         dns_zone = '{env}.{domain}'.format(**self.app_info)
 
-        app_details = self.get_app_detail()
+        dns_elb = self.generated.dns()['elb']
+        dns_elb_aws = self.get_app_aws_elb()
 
         # get correct hosted zone
         zones = self.r53client.list_hosted_zones_by_name(DNSName=dns_zone)
@@ -109,11 +85,12 @@ class SpinnakerDns:
                     self.log.info('Adding DNS record to %s zone', zone['Id'])
                     zone_ids.append(zone['Id'])
 
-        self.log.info('Updating Application URL: %s', app_details['dns_elb'])
+        self.log.info('Updating Application URL: %s', dns_elb)
 
         # This is what will be added to DNS
         dns_json = get_template(template_file='dns_upsert_template.json',
-                                data=app_details, )
+                                dns_elb=dns_elb,
+                                dns_elb_aws=dns_elb_aws)
 
         for zone_id in zone_ids:
             self.log.debug('zone_id: %s', zone_id)
@@ -124,4 +101,4 @@ class SpinnakerDns:
 
             self.log.debug('Dns upsert response: %s', pformat(response))
 
-        return app_details['dns_elb']
+        return dns_elb
