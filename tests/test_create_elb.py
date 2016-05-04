8065856a19594310220b6a9b452ca37dcfdaac1c
Nate Tangsurat <ntangsurat@gogoair.com>
2016-05-04 12:37:46 -0500

    tests: Update splay_health() test to new module

--- tests/test_create_elb.py
+++ tests/test_create_elb.py
@@ -1,28 +1,28 @@
 """Test ELB creation functions."""
-from foremast.elb.create_elb import SpinnakerELB
+from foremast.elb.splay_health import splay_health
 
 
 def test_splay():
     """Splay should split Health Checks properly."""
-    health = SpinnakerELB.splay_health('HTTP:80/test')
+    health = splay_health('HTTP:80/test')
     assert health.path == '/test'
     assert health.port == '80'
     assert health.proto == 'HTTP'
     assert health.target == 'HTTP:80/test'
 
-    health = SpinnakerELB.splay_health('TCP:8000/test')
+    health = splay_health('TCP:8000/test')
     assert health.path == ''
     assert health.port == '8000'
     assert health.proto == 'TCP'
     assert health.target == 'TCP:8000'
 
-    health = SpinnakerELB.splay_health('HTTPS:8000/test')
+    health = splay_health('HTTPS:8000/test')
     assert health.path == '/test'
     assert health.port == '8000'
     assert health.proto == 'HTTPS'
     assert health.target == 'HTTPS:8000/test'
 
-    health = SpinnakerELB.splay_health('HTTPS:80')
+    health = splay_health('HTTPS:80')
     assert health.path == '/healthcheck'
     assert health.port == '80'
     assert health.proto == 'HTTPS'
