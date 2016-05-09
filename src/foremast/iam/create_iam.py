46f59bbf7a69496c161987fa4196050f3d857793
Nate Tangsurat <ntangsurat@gogoair.com>
2016-05-09 18:11:25 -0500

    refactor: Pull out resource_action() for reuse
    See also: PSOBAT-1531

--- src/foremast/iam/create_iam.py
+++ src/foremast/iam/create_iam.py
@@ -3,10 +3,10 @@ import collections
 import logging
 
 import boto3
-from boto3.exceptions import botocore
 
 from ..utils import get_app_details, get_properties, get_template
 from .construct_policy import construct_policy
+from .resource_action import resource_action
 
 LOG = logging.getLogger(__name__)
 
@@ -108,40 +108,5 @@ def attach_profile_to_role(client,
                                             RoleName=role_name)
         LOG.info('Added Instance Profile to Role: %s -> %s', profile_name,
                  role_name)
-
-    return True
-
-
-def resource_action(client, action='', log_format='item: %(key)s', **kwargs):
-    """Call _action_ using boto3 _client_ with _kwargs_.
-
-    This is meant for _action_ methods that will create or implicitely prove a
-    given Resource exists. The _log_failure_ flag is available for methods that
-    should always succeed, but will occasionally fail due to unknown AWS
-    issues.
-
-    Args:
-        client (botocore.client.IAM): boto3 client object.
-        action (str): Client method to call.
-        log_format (str): Generic log message format, 'Added' or 'Found' will
-            be prepended depending on the scenario.
-        **kwargs: Keyword arguments to pass to _action_ method.
-
-    Returns:
-        True upon successful completion.
-    """
-    try:
-        getattr(client, action)(**kwargs)
-        LOG.info(' '.join(('Added', log_format)), kwargs)
-    except botocore.exceptions.ClientError as error:
-        error_code = error.response['Error']['Code']
-
-        if error_code == 'AccessDenied':
-            LOG.fatal(error)
-            raise
-        elif error_code == 'EntityAlreadyExists':
-            LOG.info(' '.join(('Found', log_format)), kwargs)
-        else:
-            LOG.fatal(error)
 
     return True
