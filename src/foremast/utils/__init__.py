b47ba0342a756917c8cd69d5ccfd78f78a8f3347
Nate Tangsurat <ntangsurat@gogoair.com>
2016-05-09 21:52:21 -0500

    chore: Remove configurations in favour of consts

--- src/foremast/utils/__init__.py
+++ src/foremast/utils/__init__.py
@@ -6,7 +6,6 @@ from .find_elb import *
 from .generate_encoded_user_data import *
 from .get_all_pipelines import *
 from .get_app_details import *
-from .get_configs import *
 from .get_env_credentials import *
 from .get_pipeline_id import *
 from .get_properties import *
