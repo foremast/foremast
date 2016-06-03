470ee5afaa9dc1b5f81f5503a43287751f032cb4
Doug Campbell <dcampbell@gogoair.com>
2016-06-03 14:34:50 -0500

    added most functions, still need to add pipeline one time

--- foremast-runner.py
+++ foremast-runner.py
@@ -1,111 +1,170 @@
 #!/usr/bin/env python 
-
+import argparse
+import logging
 import os
-import subprocess
 
-from foremast import *
+from foremast import (configs, consts, iam, s3, 
+securitygroup, elb, dns, slacknotify, app, pipeline)
 import gogoutils
 
-class ForemastRunner:
+LOG = logging.getLogger(__name__)
+logging.basicConfig(format=consts.LOGGING_FORMAT)
+logging.getLogger("foremast").setLevel(logging.DEBUG)
 
-    def __init__(self, token_path=None):
-        group_base = os.environ["PROJECT"]
-        repo_base = os.environ["GIT_REPO"]
-        self.git_project = "{}/{}".format(group_base, repo_base)
-        parsed = gogoutils.Parser('$GIT_PROJECT')
+class ForemastRunner:
+    def __init__(self, group=None, repo=None, env=None,
+                region=None, email=None):
+        """ Setups the Runner for all Foremast modules
+
+        Args:
+            group (str): Gitlab group of the application
+            repo (str): The application repository name
+            env (str): Deployment environment, dev/stage/prod etc
+            region (str): AWS region (us-east-1)
+        """
+        self.group = group
+        self.repo = repo
+        self.git_project = "{}/{}".format(group, repo)
+        parsed = gogoutils.Parser(self.git_project)
         generated = gogoutils.Generator(*parsed.parse_url())
         self.app = generated.app
         self.trigger_job = generated.jenkins()['name']
-        self.token_path = token_path
+        self.git_short = generated.gitlab()['main']
+        self.gitlab_token_path = os.environ["HOME"]+ "/.aws/git.token"
+        self.raw_path = "./raw.properties"
+        self.json_path = self.raw_path + ".json"
+        self.env = env
+        self.region = region
+        self.email= email
+        self.configs = None
 
 
-    def run_pipe(name=None, full_command=None):
-        splitcmd = full_command.split(" ")
-        subprocess.call(splitcmd, shell=True)
+    def write_configs(self):
+        print('===========================')
+        print('Read application.json')
+        print('===========================')
+        if not self.gitlab_token_path:
+            raise SystemExit('Must provide private token file as well.')
+        self.configs = configs.process_git_configs(git_short=self.git_short,
+                                      token_file=self.gitlab_token_path)
+
+        configs.write_variables(app_configs=self.configs,
+                        out_file=self.raw_path,
+                        git_short=self.git_short)
 
 
-# Read application.json files
-echo "============================================="
-echo " Read application.json"
-echo "============================================="
-create-configs \
--g "$GIT_PROJECT" \
--t ~/.aws/git.token \
--o ./raw.properties
+    def create_app(self):
+        print('===========================')
+        print('Creating Spinnaker App')
+        print('===========================')
+        spinnakerapp = app.SpinnakerApp(app=self.app,
+                                        email=self.email,
+                                        project=self.group,
+                                        repo=self.repo)
+        spinnakerapp.create_app()
 
-cat ./raw.properties
-echo
 
-# source the file
-grep EUREKA ./raw.properties > ./settings.properties
-while read -r line; do declare -x "$line"; done < ./settings.properties
+    def create_pipeline(self):
+        spinnakerpipeline = pipeline.SpinnakerPipeline(app=self.app,
+                                                       trigger_job=self.trigger_job,
+                                                       prop_path=self.json_path,
+                                                       base=None,
+                                                       token_file=self.gitlab_token_path)
+        spinnakerpipeline.create_pipeline()
 
-echo "============================================="
-echo " Create Iam"
-echo "============================================="
-create-iam \
---app "$APP" \
---env "$ENV"
+    def create_iam(self):
+        print('===========================')
+        print('Create IAM')
+        print('===========================')
+        iam.create_iam_resources(env=self.env, app=self.app)
 
-echo "============================================="
-echo " Create S3"
-echo "============================================="
-create-s3 \
---app "$APP" \
---env "$ENV"
+    
+    def create_s3(self):
+        print('===========================')
+        print('Create S3')
+        print('===========================')
+        s3.init_properties(env=self.env, app=self.app)
 
-echo "============================================="
-echo "Running commands for ${REGION}:${ENV}"
-echo "============================================="
+    
+    def create_secgroups(self):
+        print('===========================')
+        print('Create Security Group')
+        print('===========================')
+        sgobj = securitygroup.SpinnakerSecurityGroup(app=self.app,
+                                                    env=self.env,
+                                                    region=self.region,
+                                                    prop_path=self.json_path)
+        sgobj.create_security_group()
 
-# Security Group
-echo "============================================="
-echo " Create Security Group"
-echo "============================================="
-create-sg \
---app "$APP" \
---env "$ENV" \
---region "$REGION"
+   
+    def create_elb(self):
+        print('===========================')
+        print('Create ELB')
+        print('===========================')
+        elbobj = elb.SpinnakerELB(app=self.app,
+                                env=self.env,
+                                region=self.region,
+                                prop_path=self.json_path)
+        elbobj.create_elb()
 
-EUREKA="${ENV^^}_APP_EUREKA_ENABLED"
-if [[ "${!EUREKA}" != "true" ]]; then
 
-    # Elb
-    echo "============================================="
-    echo " Create Elb"
-    echo "============================================="
-    ELB_TYPE=$(jq ".$ENV.elb.subnet_purpose" -r ./raw.properties.json)
+    def create_dns(self):
+        print('===========================')
+        print('Create DNS')
+        print('===========================')
+        elb_type = self.configs[self.env]['elb']['subnet_purpose']
+        dnsobj = dns.SpinnakerDns(app=self.app,
+                                     env=self.env,
+                                     region=self.region,
+                                     elb_subnet=elb_subnet)
+        dnsobj.create_elb_dns()
 
-    create-elb \
-    --app "$APP" \
-    --env "$ENV" \
-    --region "$REGION"
+    def slack_notify(self):
+        print('===========================')
+        print('Sending slack notification')
+        print('===========================')
+        if self.env.startswith("prod"):
+            notify = slacknotify.SlackNotification(app=self.app,
+                                                  env=self.env,
+                                                  prop_path=self.json_path)
+            notify.post_message()
+        else:
+            LOG.info("No slack message sent, not production environment")
 
-    # Dns
-    echo "============================================="
-    echo " Create Dns"
-    echo "============================================="
-    create-dns \
-    --app "$APP" \
-    --region "$REGION" \
-    --env "$ENV" \
-    --elb-subnet "$ELB_TYPE"
-else
 
-    echo "============================================="
-    echo " Elb and Dns not executed"
-    echo "============================================="
+    def cleanup(self):
+        os.remove(self.raw_path)
+   
 
-fi
+    def prepare_infrastructure(self):
+        """ This runs everything necessary to prepare the infrastructure in a specific env """
+        self.write_configs()
+        self.create_iam()
+        self.create_s3()
+        self.create_secgroups()
+        if not self.configs[self.env]['app']['eureka_enabled']:
+            LOG.info("No Eureka, running ELB and DNS setup")
+            self.create_elb()
+            self.create_dns()
+        else:
+            LOG.info("Eureka Enabled, skipping ELB and DNS setup")
+        self.slack_notify()
+        self.cleanup()
 
-echo "============================================="
-echo " Send Slack Notification (PROD* only)"
-echo "============================================="
-slack-notify \
-    --env "$ENV" \
-    --app "$APP"
+    def prepare_app_pipeline(self):
+        self.create_app()
+        self.write_configs()
+        self.create_pipeline()
+        self.cleanup
 
-echo "============================================="
-echo " Clean properties"
-echo "============================================="
-rm raw.properties
+    def prepare_onetime_pipeline(self):
+        return
+
+if __name__ == "__main__":
+    #foremastrunner = ForemastRunner(group="forrest", repo="edge", env='dev')
+    foremastrunner = ForemastRunner(group="forrest", repo="edge")
+    foremastrunner.prepare_app_pipeline()
+    
+
+    
+
