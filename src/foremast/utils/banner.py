5605b1286da7ec564d1536782ace1201dfd26ab9
Nate Tangsurat <ntangsurat@gogoair.com>
2016-06-03 17:10:32 -0500

    feat: Add banner painting utility

---
+++ src/foremast/utils/banner.py
@@ -0,0 +1,16 @@
+"""Prints a banner.
+
+Example:
+
+================================================================================
+                             Create Security Group
+================================================================================
+"""
+
+
+def banner(text, border='=', width=80):
+    """Center _text_ in a banner _width_ wide with _border_ characters."""
+    text_padding = '{0:^%d}' % (width)
+    print(border * width)
+    print(text_padding.format(text))
+    print(border * width)
