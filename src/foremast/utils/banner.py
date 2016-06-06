c3a7b8b95e40b7267fe662b81573d8506cedecca
Nate Tangsurat <ntangsurat@gogoair.com>
2016-06-06 13:53:23 -0500

    style: Strip, isort, YAPF

--- src/foremast/utils/banner.py
+++ src/foremast/utils/banner.py
@@ -1,20 +1,20 @@
 """Prints a banner.
- 
+
 Example:
- 
+
 ================================================================================
-                              Create Security Group
+                             Create Security Group
 ================================================================================
 """
+import logging
 
 from foremast import consts
- 
-import logging
 
 LOG = logging.getLogger(__name__)
 logging.basicConfig(format=consts.LOGGING_FORMAT)
 logging.getLogger("foremast").setLevel(logging.INFO)
- 
+
+
 def banner(text, border='=', width=80):
     """Center _text_ in a banner _width_ wide with _border_ characters."""
     text_padding = '{0:^%d}' % (width)
