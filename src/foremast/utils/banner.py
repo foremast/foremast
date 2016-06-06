309e588b8caf00625d603195dc593eb4992c86de
Doug Campbell <dcampbell@gogoair.com>
2016-06-06 10:51:33 -0500

    updated nates banner

--- src/foremast/utils/banner.py
+++ src/foremast/utils/banner.py
@@ -1,7 +1,16 @@
-def banner(notice):
-    """ Prints a banner for seperating tasks """
-    print("\n===============================")
-    print(notice)
-    print("===============================\n")
-
-
+"""Prints a banner.
+ 
+Example:
+ 
+================================================================================
+                              Create Security Group
+================================================================================
+"""
+ 
+ 
+def banner(text, border='=', width=80):
+    """Center _text_ in a banner _width_ wide with _border_ characters."""
+    text_padding = '{0:^%d}' % (width)
+    print(border * width)
+    print(text_padding.format(text))
+    print(border * width)
