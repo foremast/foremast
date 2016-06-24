810986f663b255c893d0553b7c687abe2bac6fa4
Sijis Aviles <saviles@gogoair.com>
2016-06-24 16:31:31 -0500

    Bump to v2.7
    Related: PSOBAT-1899

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt', 'rt') as reqs_file:
 
 setup(
     name='foremast',
-    version='2.6',
+    version='2.7',
     description='Tools for creating infrastructure and Spinnaker Pipelines.',
     long_description=open('README.md').read(),
     author='Gogo DevOps',
