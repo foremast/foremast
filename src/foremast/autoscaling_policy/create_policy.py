f7f7185eee287c6af0bd7760f1579b54b07c8e81
Doug Campbell <dcampbell@gogoair.com>
2016-06-30 11:13:22 -0500

    updated runner to included scaling policy stuff

--- src/foremast/autoscaling_policy/create_policy.py
+++ src/foremast/autoscaling_policy/create_policy.py
@@ -8,6 +8,14 @@ from ..utils import (get_properties, get_template)
 
 
 class AutoScalingPolicy:
+    """Creates scaling policies for Spinnaker
+
+    Args:
+        app_name: Str of the application name
+        prop_path: Str of the path to property files
+        env: Str of env to add policy
+        region: Str of region for policy
+    """
 
     def __init__(self,
                  app_name='',
@@ -25,6 +33,7 @@ class AutoScalingPolicy:
         self.settings = get_properties(properties_file=prop_path, env=self.env)
 
     def create_policy(self):
+        """ Renders the template and creates the police """
         server_group = get_server_group(self.app_name, self.env)
 
         template_kwargs = {
@@ -34,15 +43,17 @@ class AutoScalingPolicy:
             'server_group' = server_group,
             'scaling_policy' = self.settings['scaling_policy']
             }
-
+        self.log.info('Rendering Scaling Policy Template')
         rendered_template = get_template(
                     template_file='scaling_policy_template.json',
                     **template_kwargs)
 
         self.post_task(rendered_template)
+        self.log.info('Successfully created scaling policy in {0}'.format(self.env))
 
 
     def post_task(self,  payload):
+        """ Posts the rendered template to correct endpoint """
         """Sends the POST to the correct endpoint and reports results"""
         url = "{0}/applications/{1}/tasks".format(API_URL, self.app_name)
         response = requests.post(url,
@@ -51,13 +62,9 @@ class AutoScalingPolicy:
         assert response.ok, "Error creating {0} Autoscaling Policy: {1}".format(self.app_name,
                                                                                 response.text)
 
-
     def get_server_group(self):
+        """ Gets the current server group """
         response = requests.get("{0}/applications/{1}".format(API_URL, self.app_name)
         for server_group in response.json['clusters'][self.env]:
             return server_group['serverGroups'][-1]
 
-
-if __name__ == '__main__':
-    policy = AutoScalingPolicy(app_name="edgeforrest",
-                               prop_path="
