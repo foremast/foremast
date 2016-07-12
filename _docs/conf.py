1752bd8bfe5391a953a832095a8c86877ef5eb53
Doug Campbell <dcampbell@gogoair.com>
2016-07-12 15:36:13 -0500

    added mocked imports

--- _docs/conf.py
+++ _docs/conf.py
@@ -344,3 +344,15 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
+
+autodoc_mock_imports = [
+        "requests",
+        "gogo-utils",
+        "boto3",
+        "jinja2",
+        "jira",
+        "murl",
+        "pyapi-gitlab",
+        "tryagain",
+        "slacker"
+        ]
