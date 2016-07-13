7e6083be59b34519d941bb2cb113c22a503bcebd
Doug Campbell <dcampbell@gogoair.com>
2016-07-13 09:29:17 -0500

    added napoleoan to sphinx configs

--- _docs/conf.py
+++ _docs/conf.py
@@ -39,6 +39,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'sphinxcontrib.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
