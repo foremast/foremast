e040ca14045b5d78edaa2ac96d981637177a38e4
Doug Campbell <dcampbell@gogoair.com>
2016-07-15 16:10:21 -0500

    added config loader for a ~/.foremast.cfg file for gitlab and slack token

--- src/foremast/utils/post_slack_message.py
+++ src/foremast/utils/post_slack_message.py
@@ -3,19 +3,21 @@ import logging
 
 import slacker
 
+from config_loader import get_configs
+
 LOG = logging.getLogger(__name__)
 
 
-def post_slack_message(message, channel,
-        slack_token='xoxb-xxxxxxxxx'):
+def post_slack_message(message, channel):
     """Format the message and post to the appropriate slack channel.
 
     Args:
         message (str): Message to post to slack
         channel (str): Desired channel. Must start with #
-        slack_token (str): Slack API token
     """
 
+    slack_token = get_configs(section='credentials')['slack_token']
+    print(slack_token)
     LOG.debug('Slack Channel: %s\nSlack Message: %s', channel, message)
     slack = slacker.Slacker(slack_token)
     try:
@@ -23,3 +25,5 @@ def post_slack_message(message, channel,
         LOG.info('Message posted to %s', channel)
     except slacker.Error:
         LOG.info("error posted message to %s", channel)
+
+post_slack_message("test", "@dcampbell")
