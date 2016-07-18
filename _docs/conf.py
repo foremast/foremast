5f7791fa64c5c253784571bd54b7b9ad5f00ed69
Nate Tangsurat <ntangsurat@gogoair.com>
2016-07-18 16:58:39 -0500

    docs: Update Python 3 intersphinx mapping

--- _docs/conf.py
+++ _docs/conf.py
@@ -344,7 +344,7 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'https://docs.python.org/3': None}
 
 autodoc_mock_imports = [
         "requests",
