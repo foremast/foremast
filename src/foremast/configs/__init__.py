9b2819fe7a2a2c97666043a5362be9dca6b27a8f
Nate Tangsurat <ntangsurat@gogoair.com>
2016-07-19 14:14:04 -0500

    docs: Clean up foremast.configs docstring

--- src/foremast/configs/__init__.py
+++ src/foremast/configs/__init__.py
@@ -1,4 +1,8 @@
-"""Package for retrieving the necessary configuration files from Gitlab, merging them
-with existing defaults, and outputing a master settings file for consumption in other foremast modules"""
+"""Package for assembling ``foremast`` configuration files.
+
+Retrieve the necessary configuration files from GitLab, merge with the default
+templates, and output a master *settings* file to be consumed by other
+``foremast`` modules.
+"""
 from .outputs import *
 from .prepare_configs import *
