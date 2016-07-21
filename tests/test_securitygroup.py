3366dae2c5423646b173db0776d08fe8ddbd82dc
Joel Vasallo <jvasallo@gogoair.com>
2016-07-21 14:42:35 -0500

    Added testcases for security group

---
+++ tests/test_securitygroup.py
@@ -0,0 +1,29 @@
+"""Ensure Security Groups get created properly"""
+import json
+from unittest.mock import patch
+from foremast.securitygroup import SpinnakerSecurityGroup
+
+SAMPLE_JSON = """{"security_group": {
+                    "description": "something useful",
+                    "egress": "0.0.0.0/0",
+                    "elb_extras": [],
+                    "ingress": {
+                      "coreforrest": [{
+                        "start_port": 8080, "end_port": 8080,
+                        "env": "dev", "protocol": "tcp"
+                      }],
+                      "sg_apps": [{
+                        "start_port": 80, "end_port": 80, "protocol": "tcp"
+                      }]
+                    }
+                  }}"""
+
+
+@patch("foremast.securitygroup.create_securitygroup.get_properties")
+def test_create_crossaccount_securitygroup(pipeline_config):
+    """Should create SG with cross account true"""
+    pipeline_config.return_value = json.loads(SAMPLE_JSON)
+    x = SpinnakerSecurityGroup(app='edgeforrest',
+                               env='dev',
+                               region='us-east-1')
+    assert x.create_security_group() is True
