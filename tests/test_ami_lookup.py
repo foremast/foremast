ed6125f2954b0499a7c3201e93381112b83e77c7
Sijis Aviles <saviles@gogoair.com>
2016-07-21 15:53:09 -0500

    Give tests better names

--- tests/test_ami_lookup.py
+++ tests/test_ami_lookup.py
@@ -2,7 +2,7 @@
 from foremast.utils import ami_lookup
 
 
-def test_lookup():
+def test_ami_lookup():
     """Lookup should contact GitLab for JSON table and resolve."""
     assert ami_lookup(name='base_fedora').startswith('ami-xxxx')
     assert ami_lookup(region='us-west-2').startswith('ami-xxxx')
