ba04cd24a3939c0e2e6cac71d4e8ae9c9792d661
Nate Tangsurat <ntangsurat@gogoair.com>
2016-07-22 12:10:25 -0500

    fix: Move section check into value checker

--- src/foremast/consts.py
+++ src/foremast/consts.py
@@ -11,37 +11,18 @@ descending order. First found wins.
    :language: ini
 """
 import logging
-from configparser import ConfigParser
+from configparser import ConfigParser, DuplicateSectionError
 from os.path import expanduser
 
 LOG = logging.getLogger(__name__)
 
 
-def validate_section(config_handle, section):
-    """Warn when *section* is missing from configurations.
+def validate_key_values(config_handle, section, key, default=None):
+    """Warn when *key* is missing from configuration *section*.
 
     Args:
         config_handle (configparser.ConfigParser): Instance of configurations.
         section (str): Name of configuration section to retrieve.
-
-    Returns:
-        configparser.SectionProxy: Configuration *section*.
-    """
-    setting = None
-    try:
-        setting = config_handle[section]
-    except KeyError:
-        LOG.warning('Section missing from configurations: [%s]', section)
-        config_handle.add_section(section)
-        setting = config_handle[section]
-    return setting
-
-
-def validate_key_values(section_handle, key, default=None):
-    """Warn when *key* is missing from configuration *section*.
-
-    Args:
-        section_dict (dict): Key/value entries for a configuration section.
         key (str): Configuration key to look up.
         default (object): Default object to use when *key* is not found.
 
@@ -49,10 +30,19 @@ def validate_key_values(section_handle, key, default=None):
         object: ``str`` when *key* exists, otherwise *default* object.
     """
     try:
+        config_handle.add_section(section)
+        LOG.warning('Section missing from configurations: [%s]', section)
+    except DuplicateSectionError:
+        pass
+
+    section_handle = config[section]
+
+    try:
         value = section_handle[key]
     except KeyError:
         LOG.warning('[%s] missing key "%s", using %r.', section_handle.name, key, default)
         value = default
+
     return value
 
 
@@ -82,20 +72,17 @@ def find_config():
 
 config = find_config()
 
-BASE_SECTION = validate_section(config, 'base')
-CREDENTIALS_SECTION = validate_section(config, 'credentials')
-WHITELISTS_SECTION = validate_section(config, 'whitelists')
+API_URL = validate_key_values(config, 'base', 'gate_api_url')
+GIT_URL = validate_key_values(config, 'base', 'git_url')
+DOMAIN = validate_key_values(config, 'base', 'domain')
+ENVS = set(validate_key_values(config, 'base', 'envs', default='').split(','))
+REGIONS = set(validate_key_values(config, 'base', 'regions', default='').split(','))
+AMI_JSON_URL = validate_key_values(config, 'base', 'ami_json_url')
 
-API_URL = validate_key_values(BASE_SECTION, 'gate_api_url')
-GIT_URL = validate_key_values(BASE_SECTION, 'git_url')
-DOMAIN = validate_key_values(BASE_SECTION, 'domain')
-ENVS = set(validate_key_values(BASE_SECTION, 'envs', default='').split(','))
-REGIONS = set(validate_key_values(BASE_SECTION, 'regions', default='').split(','))
+GITLAB_TOKEN = validate_key_values(config, 'credentials', 'gitlab_token')
+SLACK_TOKEN = validate_key_values(config, 'credentials', 'slack_token')
 
-GITLAB_TOKEN = validate_key_values(CREDENTIALS_SECTION, 'gitlab_token')
-SLACK_TOKEN = validate_key_values(CREDENTIALS_SECTION, 'slack_token')
-
-ASG_WHITELIST = set(validate_key_values(WHITELISTS_SECTION, 'asg_whitelist', default='').split(','))
+ASG_WHITELIST = set(validate_key_values(config, 'whitelists', 'asg_whitelist', default='').split(','))
 
 HEADERS = {
     'accept': '*/*',
