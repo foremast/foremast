b0fdc28d8843ea169dcf40f13685cbcda445c5d5
Nate Tangsurat <ntangsurat@gogoair.com>
2016-07-25 10:38:29 -0500

    test: Start testing around ami_lookup()
    Need to rename `foremast.utils.ami_lookup` the module. Mocking can't
    target the module when `ami_lookup()` is masking the path.

--- tests/test_ami_lookup.py
+++ tests/test_ami_lookup.py
@@ -1,8 +1,20 @@
 """Ensure AMI names can be translated."""
+import base64
+import json
+from unittest import mock
+
 from foremast.utils import ami_lookup
 
 
-def test_ami_lookup():
+@mock.patch('foremast.utils._ami_lookup.GITLAB_TOKEN')
+@mock.patch('foremast.utils._ami_lookup.gitlab.Gitlab')
+def test_lookup(gitlab, token):
     """Lookup should contact GitLab for JSON table and resolve."""
+    sample_dict = {
+        'base_fedora': 'ami-xxxx',
+        'tomcat8': 'ami-xxxx',
+    }
+    sample_json = json.dumps(sample_dict)
+    gitlab.return_value.getfile.return_value = {'content': base64.b64encode(sample_json.encode())}
     assert ami_lookup(name='base_fedora').startswith('ami-xxxx')
     assert ami_lookup(region='us-west-2').startswith('ami-xxxx')
