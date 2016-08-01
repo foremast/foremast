a3c99be7d314ab52b6bb99b550bcb61b608de15d
Sijis Aviles <saviles@gogoair.com>
2016-08-01 12:33:22 -0500

    Licensing as Apache 2.0

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
+#   Foremast - Pipeline Tooling
+#
+#   Copyright 2016 Gogo, LLC
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 #!/usr/bin/env python3
 from setuptools import find_packages, setup
 
@@ -19,13 +35,13 @@ setup(
     url='https://github.com/gogoair/foremast',
     download_url='https://github.com/gogoair/foremast',
     platforms=['OS Independent'],
-    license='MIT',
+    license='Apache License (2.0)',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
     ],
     entry_points={
@@ -51,4 +67,5 @@ setup(
             'rebuild_pipelines=foremast.runner:rebuild_pipelines',
             'slack-notify=foremast.slacknotify.__main__:main',
         ]
-    }, )
+    },
+)
