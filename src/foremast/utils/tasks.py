2ff7771b59d2adaddcbe60467ec98f176f8b6abe
Doug Campbell <dcampbell@gogoair.com>
2016-08-01 08:55:50 -0500

    moved functions to tasks.py, updated response variable name

--- src/foremast/utils/tasks.py
+++ src/foremast/utils/tasks.py
@@ -1,6 +1,7 @@
-"""Check Taskid status."""
+"""POST a new task or check status of running task"""
 import logging
 
+import json
 import requests
 from tryagain import retries
 
@@ -9,6 +10,32 @@ from ..exceptions import SpinnakerTaskError
 
 LOG = logging.getLogger(__name__)
 
+def post_task(task_data):
+    """POST JSON to Spinnaker /tasks.
+
+    Args:
+        task_data (str): the task json that needs posted.
+
+    Returns:
+        str: taskid.
+    """
+
+    url = '{}/tasks'.format(API_URL)
+
+    if isinstance(task_data, str):
+        task_json = task_data
+    else:
+        task_json = json.dumps(task_data)
+
+    resp = requests.post(url, data=task_json, headers=HEADERS)
+    resp_json = resp.json()
+
+    LOG.debug(resp_json)
+
+    assert resp.ok, 'Spinnaker communication error: {0}'.format(
+        resp.text)
+
+    return resp_json['ref']
 
 @retries(max_attempts=50, wait=2, exceptions=(AssertionError, ValueError))
 def check_task(taskid):
