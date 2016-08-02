c4d9d74a8005005a18c8b7600c072d21f069d101
Sijis Aviles <saviles@gogoair.com>
2016-08-02 14:29:41 -0500

    Add utils.banners test case

---
+++ tests/test_utils.py
@@ -0,0 +1,26 @@
+#   Foremast - Pipeline Tooling
+#
+#   Copyright 2016 Gogo, LLC
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+"""Test utils."""
+
+from unittest import mock
+from foremast.utils.banners import banner
+
+
+@mock.patch('foremast.utils.banners.LOG')
+def test_utils_banner(mock_log):
+    banner('test', border='+', width=10)
+    mock_log.info.assert_called_with('+' * 10)
