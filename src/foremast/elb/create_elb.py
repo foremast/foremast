6bdfa82c95e4c6c6845372b4e49afd5f47749235
Doug Campbell <dcampbell@gogoair.com>
2016-08-02 15:57:20 -0500

    renamed template files to remove redundant naming

--- src/foremast/elb/create_elb.py
+++ src/foremast/elb/create_elb.py
@@ -103,7 +103,7 @@ class SpinnakerELB:
         }
 
         rendered_template = get_template(
-            template_file='infrastructure/elb_data_template.json',
+            template_file='infrastructure/elb_data.json',
             **template_kwargs)
 
         return rendered_template
