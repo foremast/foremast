dc85206aa4d3b6d75247e7cc499cb9729f946591
Sijis Aviles <saviles@gogoair.com>
2016-08-04 13:11:21 -0500

    Add additional subnets tests cases

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -232,12 +232,19 @@ def test_utils_subnets_get_subnets(mock_requests_get):
     data = [
         {'vpcId': 100, 'account': 'dev', 'region': 'us-east-1',
          'target': 'ec2', 'availabilityZone': []},
+        {'vpcId': 101, 'account': 'dev', 'region': 'us-west-2',
+         'target': 'ec2', 'availabilityZone': ['us-west-2a', 'us-west-2b']},
     ]
     mock_requests_get.return_value.json.return_value = data
 
     # default - happy path
     result = get_subnets(env='dev', region='us-east-1')
     assert result == {'us-east-1': [[]]}
+
+    # default - happy path w/az
+    mock_requests_get.return_value.json.return_value = data
+    result = get_subnets(env='dev', region='')
+    assert result == {'dev': {'us-west-2': [['us-west-2a', 'us-west-2b']], 'us-east-1': [[]]}}
 
     # subnet not found
     with pytest.raises(SpinnakerSubnetError):
