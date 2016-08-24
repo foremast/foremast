ec11e6f8efa7a5e8a34bef3ac7df70c354eff903
Nate Tangsurat <ntangsurat@gogoair.com>
2016-08-24 10:02:05 -0500

    test: API Gateway create resource
    See also: PSOBAT-2337

---
+++ tests/apigateway/test_api.py
@@ -0,0 +1,17 @@
+"""Test API Gateway functions."""
+from unittest import mock
+
+from foremast.awslambda.api_gateway_event.api_gateway_event import APIGateway
+
+
+@mock.patch('foremast.awslambda.api_gateway_event.api_gateway_event.boto3')
+@mock.patch('foremast.awslambda.api_gateway_event.api_gateway_event.get_details')
+@mock.patch('foremast.awslambda.api_gateway_event.api_gateway_event.get_env_credential')
+@mock.patch('foremast.awslambda.api_gateway_event.api_gateway_event.get_properties')
+def test_create_resource(get_properties, get_env_credential, get_details, boto3):
+    """Check creating API Resource."""
+    test = APIGateway()
+    test.api_id = ''
+    test.create_resource()
+
+    test.client.create_resource.assert_called_with(restApiId='', parentId='', pathPart='/')
