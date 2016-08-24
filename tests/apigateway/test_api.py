2d2285ea0a382e9cc0ca767622cb0f2680eab180
Nate Tangsurat <ntangsurat@gogoair.com>
2016-08-24 10:28:38 -0500

    test: Add basic APIGateway init
    See also: PSOBAT-2337

--- tests/apigateway/test_api.py
+++ tests/apigateway/test_api.py
@@ -3,6 +3,18 @@ from unittest import mock
 
 from foremast.awslambda.api_gateway_event.api_gateway_event import APIGateway
 
+TEST_RULES = {'api_name': 1, 'method': 'PUT'}
+
+
+@mock.patch('foremast.awslambda.api_gateway_event.api_gateway_event.boto3')
+@mock.patch('foremast.awslambda.api_gateway_event.api_gateway_event.get_details')
+@mock.patch('foremast.awslambda.api_gateway_event.api_gateway_event.get_env_credential')
+@mock.patch('foremast.awslambda.api_gateway_event.api_gateway_event.get_properties')
+def test_apigateway(get_properties, get_env_credential, get_details, boto3):
+    """Check basic object initialization."""
+    test = APIGateway(rules=TEST_RULES)
+    assert test
+
 
 @mock.patch('foremast.awslambda.api_gateway_event.api_gateway_event.boto3')
 @mock.patch('foremast.awslambda.api_gateway_event.api_gateway_event.get_details')
@@ -10,9 +22,7 @@ from foremast.awslambda.api_gateway_event.api_gateway_event import APIGateway
 @mock.patch('foremast.awslambda.api_gateway_event.api_gateway_event.get_properties')
 def test_create_resource(get_properties, get_env_credential, get_details, boto3):
     """Check creating API Resource."""
-    test_rules = {'api_name': 1, 'method': 'PUT'}
-
-    test = APIGateway(rules=test_rules)
+    test = APIGateway(rules=TEST_RULES)
     test.api_id = ''
     test.create_resource()
 
