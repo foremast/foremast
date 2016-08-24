94eafbd3f10537c268422c08c4a8a086116bdff7
Doug Campbell <dcampbell@gogoair.com>
2016-08-24 14:48:36 -0500

    added lambda permissions for SNS event

--- src/foremast/awslambda/sns_event/sns_event.py
+++ src/foremast/awslambda/sns_event/sns_event.py
@@ -1,7 +1,7 @@
 import logging
 import boto3
 
-from ...utils import (get_sns_topic_arn, get_lambda_arn)
+from ...utils import (get_sns_topic_arn, get_lambda_arn, add_lambda_permissions, get_env_credential)
 
 LOG = logging.getLogger(__name__)
 
@@ -16,6 +16,17 @@ def create_sns_event(app_name, env, region, rules):
     lambda_arn = get_lambda_arn(app=app_name, account=env, region=region)
     topic_arn = get_sns_topic_arn(topic_name=topic_name, account=env, region=region)
     protocol = 'lambda'
+
+    statement_id = '{}_sns_{}'.format(app_name, topic_name)
+    principal = 'sns.amazonaws.com'
+    print(topic_arn)
+    add_lambda_permissions(function=app_name,
+                           statement_id=statement_id,
+                           action='lambda:InvokeFunction',
+                           principal=principal,
+                           source_arn=topic_arn,
+                           env=env,
+                           region=region)
 
     sns_client.subscribe(TopicArn=topic_arn, Protocol=protocol, Endpoint=lambda_arn)
     LOG.debug("SNS Lambda event created")
