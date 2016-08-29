75dda5d9852019b160e3b8c956cb44624242761f
Doug Campbell <dcampbell@gogoair.com>
2016-08-29 13:08:08 -0500

    updated tag to 2.14

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open('requirements.txt', 'rt') as reqs_file:
 
 setup(
     name='foremast',
-    version='2.13',
+    version='2.14',
     description='Tools for creating infrastructure and Spinnaker Pipelines.',
     long_description=open('README.rst').read(),
     author='Gogo DevOps',
