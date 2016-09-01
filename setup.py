f6108c3f26d523f49a34ef5556b3c09f19deada5
Nate Tangsurat <ntangsurat@gogoair.com>
2016-09-01 13:40:22 -0500

    chore: v2.16.3

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open('requirements.txt', 'rt') as reqs_file:
 
 setup(
     name='foremast',
-    version='2.16.2',
+    version='2.16.3',
     description='Tools for creating infrastructure and Spinnaker Pipelines.',
     long_description=open('README.rst').read(),
     author='Gogo DevOps',
