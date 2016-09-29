4de89b9aceb1d553d6fc77b729cf91aef047e4fa
Nate Tangsurat <ntangsurat@gogoair.com>
2016-09-29 14:28:39 -0500

    style: isort, YAPF
    See also: #72

--- src/foremast/utils/lookups.py
+++ src/foremast/utils/lookups.py
@@ -13,7 +13,6 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-
 """Lookup AMI ID from a simple name."""
 import json
 import logging
@@ -22,7 +21,7 @@ from base64 import b64decode
 import gitlab
 import requests
 
-from ..consts import GIT_URL, GITLAB_TOKEN, AMI_JSON_URL
+from ..consts import AMI_JSON_URL, GIT_URL, GITLAB_TOKEN
 
 LOG = logging.getLogger(__name__)
 
@@ -42,8 +41,7 @@ def ami_lookup(region='us-east-1', name='tomcat8'):
     if AMI_JSON_URL:
         LOG.info("Getting AMI from %s", AMI_JSON_URL)
         response = requests.get(AMI_JSON_URL)
-        assert response.ok, "Error getting ami info from {}".format(
-            AMI_JSON_URL)
+        assert response.ok, "Error getting ami info from {}".format(AMI_JSON_URL)
 
         ami_dict = response.json()
         LOG.debug('Lookup AMI table: %s', ami_dict)
@@ -54,8 +52,7 @@ def ami_lookup(region='us-east-1', name='tomcat8'):
         server = gitlab.Gitlab(GIT_URL, token=GITLAB_TOKEN)
         project_id = server.getproject('devops/ansible')['id']
 
-        ami_blob = server.getfile(project_id, 'scripts/{0}.json'.format(region),
-                                  'master')
+        ami_blob = server.getfile(project_id, 'scripts/{0}.json'.format(region), 'master')
         ami_contents = b64decode(ami_blob['content']).decode()
         ami_dict = json.loads(ami_contents)
         LOG.debug('Lookup AMI table: %s', ami_dict)
