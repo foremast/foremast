5e8242300e6475a9356d71cab501b286447bba01
Nate Tangsurat <ntangsurat@gogoair.com>
2016-10-09 02:37:05 -0500

    feat: Add pipeline subcommand

--- src/foremast/__main__.py
+++ src/foremast/__main__.py
@@ -2,7 +2,7 @@
 import argparse
 import logging
 
-from .args import add_debug
+from .args import add_debug, add_env
 from .consts import LOGGING_FORMAT
 
 LOG = logging.getLogger(__name__)
@@ -12,6 +12,21 @@ def add_infra(subparsers):
     """Infrastructure subcommands."""
     infra_parser = subparsers.add_parser('infra', help=add_infra.__doc__)
     infra_parser.set_defaults(func=infra_parser.print_help)
+
+
+def add_pipeline(subparsers):
+    """Pipeline subcommands."""
+    pipeline_parser = subparsers.add_parser('pipeline', help=add_pipeline.__doc__)
+    pipeline_parser.set_defaults(func=pipeline_parser.print_help)
+
+    pipeline_subparsers = pipeline_parser.add_subparsers(title='Preparers')
+
+    pipeline_full_parser = pipeline_subparsers.add_parser('app', help='Create Pipelines for an application')
+    pipeline_full_parser.set_defaults(func=pipeline_full_parser.print_help)
+
+    pipeline_onetime_parser = pipeline_subparsers.add_parser('onetime', help='Create onetime Pipeline')
+    pipeline_onetime_parser.set_defaults(func=pipeline_onetime_parser.print_help)
+    add_env(pipeline_onetime_parser)
 
 
 def main(manual_args=None):
@@ -25,6 +40,7 @@ def main(manual_args=None):
     subparsers = parser.add_subparsers(title='Commands', description='Available activies')
 
     add_infra(subparsers)
+    add_pipeline(subparsers)
 
     args, args_list = parser.parse_known_args(args=manual_args)
 
