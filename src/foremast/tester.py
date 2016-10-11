239016a9e17f264f6356ea3d92d29a877e6057aa
Nate Tangsurat <ntangsurat@gogoair.com>
2016-10-11 10:16:15 -0500

    feat: Test Gate connection

--- src/foremast/tester.py
+++ src/foremast/tester.py
@@ -1,9 +1,23 @@
 """Spinnaker tester functions."""
 import logging
 
+from .consts import API_URL
+from .utils.credentials import get_env_credential
+
 LOG = logging.getLogger(__name__)
+
+
+def gate_connection():
+    """Check Gate connection."""
+    try:
+        credentials = get_env_credential()
+        LOG.debug('Found credentials: %s', credentials)
+        LOG.info('Gate working.')
+    except TypeError:
+        LOG.fatal('Gate connection not valid: API_URL = %s', API_URL)
 
 
 def all_tests():
     """Run all tests."""
     LOG.info('Running all tests.')
+    gate_connection()
