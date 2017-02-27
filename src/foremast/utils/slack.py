7cdb4959017c112f903891a1859e5dfde62b465f
Bekzot Azimov <bazimov@gogoair.com>
2017-02-27 12:27:05 -0600

    Give a name and icon for slackbot.

--- src/foremast/utils/slack.py
+++ src/foremast/utils/slack.py
@@ -35,7 +35,7 @@ def post_slack_message(message, channel):
     LOG.debug('Slack Channel: %s\nSlack Message: %s', channel, message)
     slack = slacker.Slacker(SLACK_TOKEN)
     try:
-        slack.chat.post_message(channel, message)
+        slack.chat.post_message(channel=channel, text=message, username='pipeline-bot', icon_emoji=':gear:')
         LOG.info('Message posted to %s', channel)
     except slacker.Error:
         LOG.info("error posted message to %s", channel)
