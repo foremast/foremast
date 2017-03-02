f5fbd0a7f2f51074966470b9d4fa31c20e30a4c7
Doug Campbell <dcampbell@gogoair.com>
2017-03-02 13:44:45 -0600

    added policy attachment and updated default configs to include S3

--- src/foremast/s3deploy/s3deployments.py
+++ src/foremast/s3deploy/s3deployments.py
@@ -14,11 +14,12 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+import json
 import logging
 
 import boto3
 
-from ..utils import get_properties
+from ..utils import get_properties, get_details
 
 LOG = logging.getLogger(__name__)
 
@@ -26,12 +27,37 @@ class S3Deployments(object):
     """Handles infrastructure around depolying static content to S3"""
 
     def __init__(self, app, env, region, prop_path):
+        """S3 deployment object.
+
+        Args:
+            app (str): Application name
+            env (str): Environment/Account
+            region (str): AWS Region
+            prop_path (str): Path of environment property file
+        """
         self.app_name = app
         self.env = env
         self.region = region
-        self.properties = get_properties(prop_path)
         boto_sess = boto3.session.Session(profile_name=env)
         self.s3client = boto_sess.client('s3')
+        generated = get_details(app=app, env=env)
+        self.bucket = generated.s3_app_bucket()
+        properties = get_properties(prop_path)
+        self.s3props = properties[self.env]['s3']
 
     def create_bucket(self):
-        print("TEST")
+        """Creates or updates bucket based on app name"""
+        resp = self.s3client.create_bucket(
+            ACL=self.s3props['bucket_acl'],
+            Bucket=self.bucket
+        )
+        LOG.info('%s - S3 Bucket Upserted', self.bucket)
+        if self.s3props['bucket_policy']:
+            self._attach_bucket_policy()
+
+    def _attach_bucket_policy(self):
+        """attaches a bucket policy to app bucket"""
+        policy_str = json.dumps(self.s3props['bucket_policy'])
+        resp = self.s3client.put_bucket_policy(Bucket=self.bucket,
+                                               Policy=policy_str)
+        LOG.info('S3 Bucket Policy Attached')
