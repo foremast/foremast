62980033117831c29a3339578eade17b6bff5056
Doug Campbell <dcampbell@gogoair.com>
2017-03-09 10:27:22 -0600

    added bucket DNS

--- src/foremast/s3/s3apps.py
+++ src/foremast/s3/s3apps.py
@@ -19,7 +19,7 @@ import logging
 
 import boto3
 
-from ..utils import get_properties, get_details
+from ..utils import get_properties, get_details, get_dns_zone_ids, update_dns_zone_record
 
 LOG = logging.getLogger(__name__)
 
@@ -40,10 +40,10 @@ class S3Apps(object):
         self.region = region
         boto_sess = boto3.session.Session(profile_name=env)
         self.s3client = boto_sess.client('s3')
-        generated = get_details(app=app, env=env)
-        self.bucket = generated.s3_app_bucket()
-        properties = get_properties(prop_path)
-        self.s3props = properties[self.env]['s3']
+        self.generated = get_details(app=app, env=env)
+        self.bucket = self.generated.s3_app_bucket()
+        self.properties = get_properties(prop_path)
+        self.s3props = self.properties[self.env]['s3']
 
     def create_bucket(self):
         """Creates or updates bucket based on app name"""
@@ -56,6 +56,7 @@ class S3Apps(object):
             self._attach_bucket_policy()
         if self.s3props['website']['enabled']:
             self._set_website_settings()
+            self._set_bucket_dns()
 
     def _attach_bucket_policy(self):
         """attaches a bucket policy to app bucket"""
@@ -72,3 +73,24 @@ class S3Apps(object):
         resp = self.s3client.put_bucket_website(Bucket=self.bucket,
                                                 WebsiteConfiguration=website_config)
         LOG.info('S3 website settings updated')
+
+    def _set_bucket_dns(self):
+        """Creates CNAME for s3 endpoint"""
+
+        # Different regions have different s3 endpoint formats
+        dotformat_regions =["eu-west-2", "eu-central-1", "ap-northeast-2", "ap-south-1", "ca-central-1", "us-east-2"]
+        if self.region in dotformat_regions:
+            s3_endpoint = "{0}.s3-website.{1}.amazonaws.com".format(self.bucket, self.region)
+        else:
+            s3_endpoint = "{0}.s3-website-{1}.amazonaws.com".format(self.bucket, self.region)
+
+        bucket_dns = self.generated.dns()['global']
+        zone_ids = get_dns_zone_ids(env=self.env, facing="public")
+        dns_kwargs = {'dns_name': bucket_dns,
+                      'dns_name_aws': s3_endpoint,
+                      'dns_ttl': self.properties[self.env]['dns']['ttl']}
+
+        for zone_id in zone_ids:
+            LOG.debug('zone_id: %s', zone_id)
+            update_dns_zone_record(self.env, zone_id, **dns_kwargs)
+        LOG.info("Created DNS %s for Bucket", bucket_dns)
