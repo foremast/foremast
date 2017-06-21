49796d3bd628cbcddd920f6bc15158437b9ee738
Nate Tangsurat <ntangsurat@gogoair.com>
2017-06-21 10:48:47 -0500

    tests: Check Task failure

--- tests/utils/tasks/test_check_task.py
+++ tests/utils/tasks/test_check_task.py
@@ -1,6 +1,9 @@
 """Verify :func:`foremsat.utils.tasks.check_task` functionality."""
 from unittest import mock
 
+import pytest
+
+from foremast.exceptions import SpinnakerTaskError
 from foremast.utils.tasks import _check_task
 
 FAIL_MESSAGE = 'TERMINAL'
@@ -15,3 +18,17 @@ def test_task_success(mock_requests):
     result = _check_task(taskid='')
 
     assert result == SUCCESS_MESSAGE
+
+
+@mock.patch('foremast.utils.tasks.requests')
+def test_task_failure(mock_requests):
+    """Failed Task."""
+    mock_requests.get.return_value.json.return_value = {
+        'status': FAIL_MESSAGE,
+        'execution': {
+            'stages': [],
+        },
+    }
+
+    with pytest.raises(SpinnakerTaskError):
+        _check_task(taskid='')
