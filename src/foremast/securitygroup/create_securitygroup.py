11cfbe6a29010b7aea0bd46d08e4e61291509b39
Steven Basgall <sbasgall@gogoair.com>
2017-07-12 13:50:52 -0600

    Added ability to have default application securitygroup rules

--- src/foremast/securitygroup/create_securitygroup.py
+++ src/foremast/securitygroup/create_securitygroup.py
@@ -46,7 +46,9 @@ import logging
 import boto3
 from boto3.exceptions import botocore
 
-from ..exceptions import (ForemastConfigurationFileError, SpinnakerSecurityGroupCreationFailed,
+from ..consts import DEFAULT_SECURITYGROUP_RULES
+from ..exceptions import (ForemastConfigurationFileError,
+                          SpinnakerSecurityGroupCreationFailed,
                           SpinnakerSecurityGroupError)
 from ..utils import (get_properties, get_security_group_id, get_template,
                      get_vpc_id, wait_for_task, warn_user, get_details)
@@ -217,6 +219,7 @@ class SpinnakerSecurityGroup(object):
 
         try:
             ingress = self.properties['security_group']['ingress']
+            ingress.update(DEFAULT_SECURITYGROUP_RULES)
         except KeyError:
             msg = 'Possible missing configuration for "{0}".'.format(self.env)
             self.log.error(msg)
