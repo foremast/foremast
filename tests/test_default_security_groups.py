09d36bb0fa7f454bbc4215998afce7906c42ccf0
Steven Basgall <sbasgall@gogoair.com>
2017-08-01 15:51:32 -0600

    test: added test and slight refactor for test

---
+++ tests/test_default_security_groups.py
@@ -0,0 +1,30 @@
+"""Test default Security Groups."""
+from unittest import mock
+
+from foremast.securitygroup.create_securitygroup import SpinnakerSecurityGroup
+
+
+@mock.patch('foremast.securitygroup.create_securitygroup.get_properties')
+def test_default_security_groups(mock_properties):
+    """Make sure default Security Groups are added to the ingress rules."""
+    ingress = {
+        'test_app': [
+            {
+                'start_port': 30,
+                'end_port': 30,
+            },
+        ],
+    }
+
+    mock_properties.return_value = {
+        'security_group': {
+            'ingress': ingress,
+            'description': '',
+        },
+    }
+
+    test_sg = {'myapp': [{'start_port': '22', 'end_port': '22', 'protocol': 'tcp' }]}
+    with mock.patch.dict('foremast.securitygroup.create_securitygroup.DEFAULT_SECURITYGROUP_RULES', test_sg):
+        test_sg = SpinnakerSecurityGroup()
+        ingress = test_sg.update_default_securitygroup_rules()
+        assert 'myapp' in ingress
