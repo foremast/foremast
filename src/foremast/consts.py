1aafaf99f4eefde3ee1e5f30891420b0ef9cabfd
Varun Rao <vrao@gogoair.com>
2017-08-16 16:18:23 -0500

    fix: Change name of links

--- src/foremast/consts.py
+++ src/foremast/consts.py
@@ -210,7 +210,7 @@ ASG_WHITELIST = set(validate_key_values(config, 'whitelists', 'asg_whitelist', d
 APP_FORMATS = extract_formats(config)
 GATE_CLIENT_CERT = expandvars(expanduser(validate_key_values(config, 'base', 'gate_client_cert', default='')))
 GATE_CA_BUNDLE = expandvars(expanduser(validate_key_values(config, 'base', 'gate_ca_bundle', default='')))
-LINKS = _convert_string_to_native(validate_key_values(config, 'internal_tools', 'links', {}))
+LINKS = _convert_string_to_native(validate_key_values(config, 'urls', 'spinnaker_links', {}))
 
 HEADERS = {
     'accept': '*/*',
