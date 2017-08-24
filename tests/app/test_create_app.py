ec372b2e10c8baa8bdf29e92b4599db28d4992fc
Varun Rao <vrao@gogoair.com>
2017-08-23 23:51:16 -0500

    tests: Unit Tests for app creation

--- tests/app/test_create_app.py
+++ tests/app/test_create_app.py
@@ -12,9 +12,8 @@ def test_instance_links():
     """Checks if instance_links is being populated properly"""
     pipeline_config = {
         "instance_links": {
-            "test3": "https://test3.com",
+            "test3": "https://test2.com",
             "test4": "https://test4.com",
-            "test5": "https://test2.com"
         }
     }
     spinnaker_app = SpinnakerApp(pipeline_config=pipeline_config)
@@ -24,16 +23,16 @@ def test_instance_links():
     pipeline_config = { "instance_links": {} }
     spinnaker_app = SpinnakerApp(pipeline_config=pipeline_config)
     instance_links = spinnaker_app.retrieve_instance_links()
-    assert instance_links == {"test1": "https://test1.com", "test2": "https://test2.com"},
-                             "Instance Links are not being retrieved properly"
+    assert instance_links == {"test1": "https://test1.com", "test2": "https://test2.com"}, "Instance Links are not being retrieved properly"
 
 
 @mock.patch('foremast.app.create_app.API_URL', new='https://spinnaker.build.gogoair.com:7777')
 @mock.patch.object(SpinnakerApp, 'retrieve_instance_links', return_value={"test1": "https://test1.com",
-                   "test2": "https://test2.com"}, "test3")
+                   "test2": "https://test2.com"})
 def test_retrieval_of_templates(mock_instance_links):
     """Checks to see if the instance links are populating app_data.json.j2 properly.
     This also mocks the return_value of the accounts on Spinnaker"""
+
     pipeline_config = {
         "instance_links": {
             "health": ":8080/health",
@@ -59,7 +58,12 @@ def test_retrieval_of_templates(mock_instance_links):
     mock_api_url = 'https://spinnaker.build.gogoair.com:7777/credentials'
     spinnaker_app = SpinnakerApp(pipeline_config=pipeline_config, app=app, email=email, 
                                  project=project, repo=repo)
-
+    accounts = [
+        {
+            "type": "aws",
+            "name": "dev",
+        }
+    ]
     with requests_mock.Mocker() as mock_requests:
         mock_requests.get(mock_api_url, json=accounts)    
         accounts = spinnaker_app.get_accounts()
@@ -69,5 +73,4 @@ def test_retrieval_of_templates(mock_instance_links):
     assert_links = {}
     for instance_link in instance_links:
         assert_links[instance_link['title']] = instance_link['path']
-    assert assert_links == {**pipeline_config['instance_links'], **mock_instance_links.return_value},
-                            "instance_links in app_data.json.j2 is not being populated properly"
+    assert assert_links == {**pipeline_config['instance_links'], **mock_instance_links.return_value}, "instance_links in app_data.json.j2 is not being populated properly"
