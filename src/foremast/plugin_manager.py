f220720aa229d545555fc4690e960f9972443378
Sijis Aviles <saviles@gogoair.com>
2017-10-12 22:42:36 -0500

    chore: Add docstring to plugin manager

--- src/foremast/plugin_manager.py
+++ src/foremast/plugin_manager.py
@@ -1,7 +1,15 @@
+"""Manager to handle plugins"""
 from pluginbase import PluginBase
 
 
 class PluginManager:
+    """Class to manage and create Spinnaker applications
+
+    Args:
+        paths (str): Path of plugin directory.
+        provider (str): The name of the cloud provider.
+    """
+
     def __init__(self, paths, provider):
         self.paths = [paths]
         self.provider = provider
@@ -10,8 +18,10 @@ class PluginManager:
         self.plugin_source = plugin_base.make_plugin_source(searchpath=self.paths)
 
     def plugins(self):
+        """List of all plugins available."""
         for plugin in self.plugin_source.list_plugins():
             yield plugin
 
     def load(self):
+        """Load the plugin object."""
         return self.plugin_source.load_plugin(self.provider)
