989dd985d51606cb916fb39803eb66e286b121b9
Sijis Aviles <saviles@gogoair.com>
2017-10-18 14:47:10 -0500

    test: Combined instance links

--- tests/app/test_create_app.py
+++ tests/app/test_create_app.py
@@ -8,15 +8,19 @@ MANAGER = PluginManager('app', 'aws')
 PLUGIN = MANAGER.load()
 
 
-@mock.patch('foremast.app.aws.base.LINKS', new={"test1": "https://test1.com", "test2": "https://test2.com"})
+@mock.patch('foremast.app.aws.base.LINKS', new={'example1': 'https://example1.com'})
 def test_default_instance_links():
     """Validate default instance_links are being populated properly."""
     pipeline_config = {
         "instance_links": {
-            "test3": "https://test2.com",
-            "test4": "https://test4.com",
+            "example2": "https://example2.com",
         }
     }
+
+    combined = {'example1': 'https://example1.com'}
+    combined.update(pipeline_config['instance_links'])
+
     spinnaker_app = PLUGIN.SpinnakerApp(pipeline_config=pipeline_config)
     instance_links = spinnaker_app.retrieve_instance_links()
-    assert instance_links == {"test1": "https://test1.com"}, "Instance Links are not being retrieved properly"
+
+    assert instance_links == combined, "Instance Links are not being retrieved properly"
