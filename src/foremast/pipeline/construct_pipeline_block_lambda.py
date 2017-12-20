82ff26478bf41e0b99d5ab62d256111d1b6cf197
Joel Vasallo <joelvasallo@gmail.com>
2017-12-20 15:24:36 -0600

    Revert "Added support for removing duplicate security group references based on name"
    This reverts commit 57123698f68d94ce8ef34f3380ac1f327707de91.

--- src/foremast/pipeline/construct_pipeline_block_lambda.py
+++ src/foremast/pipeline/construct_pipeline_block_lambda.py
@@ -19,7 +19,7 @@ import json
 import logging
 from pprint import pformat
 
-from ..consts import DEFAULT_EC2_SECURITYGROUPS, SECURITYGROUP_REPLACEMENTS
+from ..consts import DEFAULT_EC2_SECURITYGROUPS
 from ..utils import generate_encoded_user_data, get_template
 
 LOG = logging.getLogger(__name__)
@@ -67,11 +67,6 @@ def construct_pipeline_block_lambda(env='',
     instance_security_groups = sorted(DEFAULT_EC2_SECURITYGROUPS[env])
     instance_security_groups.append(gen_app_name)
     instance_security_groups.extend(settings['security_group']['instance_extras'])
-
-    for each_sg, duplicate_sg_name in SECURITYGROUP_REPLACEMENTS.items():
-        if each_sg in security_groups and duplicate_sg_name in security_groups:
-            LOG.info('Duplicate SG found. Removing %s in favor of %s.', duplicate_sg_name, each_sg)
-            instance_security_groups.remove(duplicate_sg_name)
 
     LOG.info('Instance security groups to attach: %s', instance_security_groups)
 
