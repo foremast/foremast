ac5af5ddae81e123ac1f2505eba90c5352817ade
linjmeyer <lin@linmeyer.net>
2019-08-07 11:46:34 -0500

    Added pipeline cleanup for k8s

--- src/foremast/pipeline/create_pipeline_kubernetes.py
+++ src/foremast/pipeline/create_pipeline_kubernetes.py
@@ -17,10 +17,10 @@
 import json
 from pprint import pformat
 import requests
-from ..utils import get_template
+from ..utils import check_managed_pipeline, get_all_pipelines, get_template
 from ..exceptions import SpinnakerPipelineCreationFailed
+from .clean_pipelines import delete_pipeline
 from ..consts import API_URL, GATE_CA_BUNDLE, GATE_CLIENT_CERT, DEFAULT_RUN_AS_USER
-# from .clean_pipelines import clean_pipelines
 from .create_pipeline import SpinnakerPipeline
 
 
@@ -58,11 +58,12 @@ class SpinnakerPipelineKubernetesPipeline(SpinnakerPipeline):
         3. Renders all of the pipeline blocks as defined in configs
         4. Runs post_pipeline to create pipeline
         """
-        # ToDo: Taken out until we sort out the region/env/account mappings from foremast->spin->k8s
-        # clean_pipelines(app=self.app_name, settings=self.settings)
 
         pipeline_envs = self.environments
         self.log.debug('Envs from pipeline.json: %s', pipeline_envs)
+
+        deleted_pipelines = self.clean_kubernetes_pipelines(pipeline_envs)
+        self.log.debug('Deleted %s managed pipelines: %s', len(deleted_pipelines), deleted_pipelines)
 
         pipelines = {}
         for env in pipeline_envs:
@@ -89,6 +90,33 @@ class SpinnakerPipelineKubernetesPipeline(SpinnakerPipeline):
             self.post_pipeline(pipeline)
 
         return True
+
+    def clean_kubernetes_pipelines(self, environments):
+        """Deletes pipelines managed by formast that are not included in the current configuration
+        1. Checks for pipelines following the convetion: appname [environment]
+        2. Checks if the environment is not in the current environment list
+        3. If not in the environment list, the pipeline is deleted
+        Pipelines not managed by Foremast are ignored
+        """
+        deleted_pipelines = []
+        existing_pipelines = get_all_pipelines(app=self.app_name)
+        for pipeline in existing_pipelines:
+            pipeline_name = pipeline['name']
+
+            try:
+                env = check_managed_pipeline(name=pipeline_name, app_name=self.app_name)
+            except ValueError:
+                self.log.info('"%s" is not managed.', pipeline_name)
+                continue
+
+            self.log.debug('Check "%s" in defined Envs.', env)
+
+            if env not in environments:
+                delete_pipeline(app=self.app_name, pipeline_name=pipeline_name)
+                deleted_pipelines.append(pipeline_name)
+
+        return deleted_pipelines
+
 
     def generate_template_data(self, environment):
         """Generates the data used to populate the Jinja 2 template for each pipeline."""
@@ -123,12 +151,15 @@ class SpinnakerPipelineKubernetesPipeline(SpinnakerPipeline):
 
         # If they specified a canary name, add it to the jinja template data
         if 'canaryConfigName' in self.settings['pipeline']['kubernetes'].keys():
-            data['canary'] = self.create_canary_config()
-  
+            data['canary'] = self.create_canary_config()       
         return data
 
     def create_canary_config(self):
         """Creats a canary config that can be passed to the a J2 template for kayenta canaries
+        1. Checks if the pipeline.kubernetes.canaryConfigName setting was set by the user
+        2. If set, other canary configuration options like canaryAnalysisIntervalMins is added to the config
+        3. The canary config Id is resolved from Spinnaker using the name set by the user
+        It is not verified if the current app has the correct permissions to use the canary config requested.
         """
         kube_config = self.settings['pipeline']['kubernetes']
         interval_minutes = kube_config['canaryAnalysisIntervalMins']
@@ -142,7 +173,8 @@ class SpinnakerPipelineKubernetesPipeline(SpinnakerPipeline):
         return canary
 
     def get_canary_id(self, name):
-        """Finds a canary config ID matching the name passed
+        """Finds a canary config ID matching the name passed.  
+        Assumes the canary name is unique and the first match wins.
         """
         url = "{}/v2/canaryConfig".format(API_URL)
         canary_response = requests.get(url, headers=self.header, verify=GATE_CA_BUNDLE, cert=GATE_CLIENT_CERT)
