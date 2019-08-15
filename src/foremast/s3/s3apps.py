7e0195592f2277d1c08062dd8dd75a77e68852d5
Joel Vasallo <joel.vasallo@redbox.com>
2019-08-15 14:36:21 -0500

    Fixed lint issues

--- src/foremast/s3/s3apps.py
+++ src/foremast/s3/s3apps.py
@@ -226,11 +226,17 @@ class S3Apps:
         notification_config = {}
 
         if self.s3props['notification']['enabled']:
-            if 'topic_configurations' in self.s3props['notification'] and self.s3props['notification']['topic_configurations'] != [{}]:
+            if 'topic_configurations' in self.s3props['notification'] and \
+                self.s3props['notification']['topic_configurations'] != [{}]:
+                
                 notification_config['TopicConfigurations'] = self.s3props['notification']['topic_configurations']
-            if 'queue_configurations' in self.s3props['notification'] and self.s3props['notification']['queue_configurations'] != [{}]:
+            if 'queue_configurations' in self.s3props['notification'] \
+                and self.s3props['notification']['queue_configurations'] != [{}]:
+                
                 notification_config['QueueConfigurations'] = self.s3props['notification']['queue_configurations']
-            if 'lambda_configurations' in self.s3props['notification'] and self.s3props['notification']['lambda_configurations'] != [{}]:
+            if 'lambda_configurations' in self.s3props['notification'] and \
+                self.s3props['notification']['lambda_configurations'] != [{}]:
+                
                 notification_config['LambdaFunctionConfigurations'] = \
                     self.s3props['notification']['lambda_configurations']
         LOG.debug('Notification Config: %s', notification_config)
