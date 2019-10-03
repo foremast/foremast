a26bc6da807efc10a96b04543271dbc5637d5508
Joel Vasallo <joel.vasallo@redbox.com>
2019-10-03 16:07:00 -0500

    refactor: Centralizing gate calls to a single function

---
+++ src/foremast/utils/gate.py
@@ -0,0 +1,41 @@
+#   Foremast - Pipeline Tooling
+#
+#   Copyright 2019 Redbox Automated Retail, LLC
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+"""Post a message to slack."""
+import logging
+import requests
+
+from ..consts import API_URL, GATE_CA_BUNDLE, GATE_CLIENT_CERT
+
+LOG = logging.getLogger(__name__)
+
+def gate_request(method='GET', uri=None):
+    """Make a request to Gate's API via various auth methods
+
+    Args:
+        method (str): Method to request Gate API; GET or POST
+        uri (str): URI path to gate API
+    """
+    response = None
+    url = '{host}{uri}'.format(host=API_URL, uri=uri)
+
+    if method == 'GET':
+         response = requests.get(url, verify=GATE_CA_BUNDLE, cert=GATE_CLIENT_CERT)
+    elif method == 'POST':
+         response = requests.post(url, verify=GATE_CA_BUNDLE, cert=GATE_CLIENT_CERT)
+    else:
+        raise NotImplementedError
+
+    return response
