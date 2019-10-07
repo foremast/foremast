e66aa1c76e66ed3eea725b62549fcf4656aedc66
Wayne Taylor <wayne.taylor@redbox.com>
2019-10-07 13:24:36 +0200

    raise exception when no templates dir set

--- src/foremast/pipeline/create_pipeline_manual.py
+++ src/foremast/pipeline/create_pipeline_manual.py
@@ -67,6 +67,8 @@ class SpinnakerPipelineManual(SpinnakerPipeline):
             str: Contents of pipeline file."""
         # Check if this file is a shared template in the TEMPLATES_PATH
         if file_name.startswith("templates://"):
+            if TEMPLATES_PATH is None: 
+                raise Exception("Cannot use templates:// schema without TEMPLATES_PATH")
             file_name = file_name.lstrip("templates://")
             pipeline_templates_path = TEMPLATES_PATH.rstrip("/") + "/pipeline"
             lookup = FileLookup(git_short=None, runway_dir=pipeline_templates_path)
