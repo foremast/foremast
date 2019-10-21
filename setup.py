909647626f2abaff4d6d14373d3ff26a2bc9cd85
Joel Vasallo <joel.vasallo@redbox.com>
2019-10-21 14:49:45 -0500

    fix: setup.py install targets

--- setup.py
+++ setup.py
@@ -41,10 +41,9 @@ setup(
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
     ],
