903ce1683b455cdc8fa3d5e05329419a15408a52
Matias Puerta <m.puerta@stuart.com>
2019-11-26 15:02:59 +0100

    Fix lint issue

--- src/foremast/utils/gate.py
+++ src/foremast/utils/gate.py
@@ -47,7 +47,7 @@ def gate_request(method='GET', uri=None, headers={}, data={}, params={}):
 
             headers['Authorization'] = 'Bearer {}'.format(iap_response['id_token'])
             LOG.info('Successfully set Google IAP Bearer Token in Request.')
-        
+
         elif 'github' in GATE_AUTHENTICATION:
             github_token = GATE_AUTHENTICATION['github']['token']
             headers['Authorization'] = 'Bearer {}'.format(github_token)
