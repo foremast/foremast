82eaf3d29df3d9adf16af0aad9c5cee765bdf276
linjmeyer <lin@linmeyer.net>
2020-02-03 09:55:43 -0600

    feat: removed plugin system

--- src/foremast/app/__init__.py
+++ src/foremast/app/__init__.py
@@ -14,4 +14,4 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 """Package for creating a Spinnaker application."""
-from .create_app import SpinnakerApp
+from .spinnaker_app import SpinnakerApp
