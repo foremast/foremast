779e574f667b0d7886dbde24d866429ec8497f0a
linjmeyer <lin@linmeyer.net>
2020-02-24 10:40:01 +0100

    fix: reverting jinja function master commit

--- src/foremast/pipeline/jinja_functions.py
+++ src/foremast/pipeline/jinja_functions.py
@@ -24,7 +24,6 @@ def get_jinja_functions():
     functions = dict()
     functions[get_canary_id.__name__] = get_canary_id
     functions[get_pipeline_id.__name__] = get_pipeline_id
-    functions[raise_exception.__name__] = raise_exception
 
     return functions
 
@@ -38,8 +37,3 @@ def get_jinja_variables(pipeline):
     variables["repo_name"] = pipeline.repo_name
 
     return variables
-
-
-def raise_exception(message):
-    """A function for raising an exception from inside a Jinja Template"""
-    raise Exception(message)
