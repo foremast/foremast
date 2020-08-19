4b23bdbdd6eb50ead5f902d72c9bede14406eeb6
linjmeyer <lin@linmeyer.net>
2020-08-19 16:41:11 -0500

    feat: Added GCP IAM group based permissions

--- src/foremast/gcp_iam/create_iam_resources.py
+++ src/foremast/gcp_iam/create_iam_resources.py
@@ -1,5 +1,6 @@
 from googleapiclient.errors import HttpError
 
+from ..exceptions import ForemastError
 from ..utils.gcp_environment import GcpEnvironment
 from . import create_service_account, get_policy, set_policy, modify_policy_remove_member, modify_policy_add_binding
 from tryagain import retries
@@ -9,7 +10,7 @@ import logging
 LOG = logging.getLogger(__name__)
 
 
-def create_iam_resources(env: GcpEnvironment, app_name: str, services: dict = None):
+def create_iam_resources(env: GcpEnvironment, app_name: str, group_name: str, services: dict = None):
     credentials = env.get_credentials()
     # Ensure service account is created
     service_account = create_service_account(credentials=credentials,
@@ -18,7 +19,10 @@ def create_iam_resources(env: GcpEnvironment, app_name: str, services: dict = No
     member = "serviceAccount:" + service_account["email"]
 
     # Get the gcp_roles requested in pipeline.json with the full project id as the key
-    gcp_roles_by_project = _get_gcp_roles_by_project(services['gcp_roles'], env) if "gcp_roles" in services else dict()
+    if "gcp_roles" in services:
+        gcp_roles_by_project = _get_gcp_roles_by_project(services['gcp_roles'], env, group_name)
+    else:
+        gcp_roles_by_project = dict()
 
     # For each project in this GCP environment, get the policy
     # remove any references to this svc account, then re-add based on pipeline.json
@@ -64,16 +68,44 @@ def _update_policy_for_project(project_id, roles_by_project, member, credentials
         raise e
 
 
-def _get_gcp_roles_by_project(gcp_roles, env: GcpEnvironment):
+def _get_gcp_roles_by_project(gcp_roles: list, env: GcpEnvironment, group_name: str):
     """Gets the full project name for the given services.gcp_roles block
 
     Returns a dictionary where the key is the full project name, and the value
-    is the corresponding entry from services.gcp_roles"""
+    is the corresponding entry from services.gcp_roles
 
+    Raises ForemastError if a requested role is not permitted on the given project"""
     roles_by_project = dict()
 
     for role_definition in gcp_roles:
-        project = env.get_project(project_prefix=role_definition['project_prefix'])
+        project = env.get_project(project_name=role_definition['project_name'])
+        # If they are not permitted to request roles on this project, stop Foremast now:
+        if not _check_group_permitted_project(project, group_name):
+            raise ForemastError("Group '{}' is not permitted to request roles in "
+                                "project '{}'".format(group_name, project['projectId']))
+
+        # Otherwise add it to the dict of project->roles
         roles_by_project[project['projectId']] = role_definition
 
     return roles_by_project
+
+
+def _check_group_permitted_project(project, group_name):
+    """Returns true if a group is permitted to access this project based on
+    the projects FOREMAST_GROUPS=team1,team2 label.  If no label is present
+    all groups are permitted."""
+
+    if 'foremast_groups' not in project['labels'] or not project['labels']['foremast_groups'].strip():
+        LOG.debug("Project '%s' permits access from all groups", project['projectId'])
+        return True
+
+    # foremast_groups defined and not empty
+    foremast_groups_csv = project['labels']['foremast_groups']
+    LOG.debug("Project '%s' only supports foremast deployments from groups: '%s'", project['projectId'],
+              foremast_groups_csv)
+    for permitted_group in foremast_groups_csv.split(','):
+        if group_name == permitted_group.strip():
+            return True
+
+    # Default to denied
+    return False
