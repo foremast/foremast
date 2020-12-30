d22a98a67e7fb29f1ab85230ea3d52b66e3a4b1b
linjmeyer <lin@linmeyer.net>
2020-12-30 15:52:46 -0600

    fix: AWS IAM put role policy no longer fails silently

--- src/foremast/iam/resource_action.py
+++ src/foremast/iam/resource_action.py
@@ -20,21 +20,22 @@ from boto3.exceptions import botocore
 
 LOG = logging.getLogger(__name__)
 
-
-def resource_action(client, action='', log_format='item: %(key)s', **kwargs):
+def resource_action(client, action='', log_format='item: %(key)s', raise_errors=False, **kwargs):
     """Call _action_ using boto3 _client_ with _kwargs_.
 
-    This is meant for _action_ methods that will create or implicitely prove a
-    given Resource exists. The _log_failure_ flag is available for methods that
-    should always succeed, but will occasionally fail due to unknown AWS
-    issues.
+    This is meant for _action_ methods that will create or implicitly prove a
+    given Resource exists. This method will fail silently and log errors by default,
+    use the raise_errors flag to change this functionality.  AccessDenied errors will
+    always cause a raised error. EntityAlreadyExists will never raise an error.
 
     Args:
         client (botocore.client.IAM): boto3 client object.
         action (str): Client method to call.
         log_format (str): Generic log message format, 'Added' or 'Found' will
             be prepended depending on the scenario.
-        prefix (str): Prefix word to use in successful INFO message.
+        raise_errors (bool): If errors should be raised, defaults to False.
+            AccessDenied errors will always cause a raised error.
+            EntityAlreadyExists will never raise an error.
         **kwargs: Keyword arguments to pass to _action_ method.
 
     Returns:
@@ -48,7 +49,7 @@ def resource_action(client, action='', log_format='item: %(key)s', **kwargs):
     except botocore.exceptions.ClientError as error:
         error_code = error.response['Error']['Code']
 
-        if error_code == 'AccessDenied':
+        if raise_errors or error_code in "AccessDenied":
             LOG.fatal(error)
             raise
         elif error_code == 'EntityAlreadyExists':
