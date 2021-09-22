1696950f813f3b03dd4ee75b02fbb1dddc75bf23
Joel Vasallo <joel.vasallo@redbox.com>
2021-09-22 09:39:28 -0500

    fix: Missing key in tests

--- tests/lambda/test_aws.py
+++ tests/lambda/test_aws.py
@@ -23,6 +23,7 @@ TEST_PROPERTIES = {
         'lambda_tracing': None,
         'lambda_destinations': None,
         'lambda_subnet_count': None,
+        'lambda_subnet_purpose': 'internal',
         'lambda_filesystems': None,
         'lambda_provisioned_throughput': None,
     }
